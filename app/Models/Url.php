ed3bcad69f606adb516a101511a8b493885df86d
Minh-bao <mbnguyen@hotmail.fr>
2020-10-17 16:15:29 +0200

    add method to create unique shortened url

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Url extends Model
 {
     use HasFactory;
+
     public $timestamps = false;
+    protected $fillable = ['url', 'shortened'];
+
+
+    public static function getUniqueShortUrl() {
+        $shortened = Str::random(5);
+
+        if(static::whereShortened($shortened)->count() != 0) {
+            return static::getUniqueShortUrl();
+        } 
+        return $shortened;
+    }
+
 }
