98db623f563342d39dd13dfce6934d8cf515d8ec
Minh-bao <mbnguyen@hotmail.fr>
2020-10-17 17:11:52 +0200

    show last input

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         
         <form method="POST">
             {{ csrf_field() }}
-            <input type="text" name="url" placeholder="Enter your original URL">
+            <input type="text" name="url" placeholder="Enter your original URL" value="{{ old('url') }}">
             {!! $errors->first('url', '<p class="error-msg">:message</p>') !!}
             <input type="submit" value="Shorten Url">
         </form>
